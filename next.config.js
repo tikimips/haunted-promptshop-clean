c40fdb897357a029a78fce2237f701a76773ed74
tikimips <tikimips@gmail.com>
2025-09-06 19:50:54 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+module.exports = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'source.unsplash.com' },
-      { protocol: 'https', hostname: 'cdn.dribbble.com' },
       { protocol: 'https', hostname: 'picsum.photos' },
-      { protocol: 'https', hostname: 'placehold.co' },
+      // add any others you use
     ],
   },
 };
-
-module.exports = nextConfig;
